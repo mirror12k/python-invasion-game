2d83fbc1c9b91626410e9c30d858cf78effe0de0
mirror <mirror12k@gmail.com>
2017-03-26 16:30:40 -0400

    - implemented enemies firing directional bullets at the player

--- example/simple/simple_game.py
+++ example/simple/simple_game.py
@@ -7,9 +7,9 @@ import telekinesis
 import pygame
 
 
-class BulletContainer(telekinesis.gamecore.ContainerEntity):
+class PlayerBulletContainer(telekinesis.gamecore.ContainerEntity):
 	def update(self):
-		super(BulletContainer, self).update()
+		super(PlayerBulletContainer, self).update()
 		bullet_boxes = [ bullet.bounding_box for bullet in self.entities ]
 		# print "debug bullets", len(self.entities)
 		for ent in [ ent for ent in self.parent.entities if isinstance(ent, CollidingEntity) ]:
@@ -18,12 +18,25 @@ class BulletContainer(telekinesis.gamecore.ContainerEntity):
 				ent.takeDamage(self.entities[index].damage)
 				self.removeEntity(self.entities[index])
 
+class EnemyBulletContainer(telekinesis.gamecore.ContainerEntity):
+	def update(self):
+		super(EnemyBulletContainer, self).update()
+		bullet_boxes = [ bullet.bounding_box for bullet in self.entities ]
+		# print "debug bullets", len(self.entities)
+		player = self.parent.player
+		index = player.bounding_box.collidelist(bullet_boxes)
+		if index != -1:
+			player.takeDamage(self.entities[index].damage)
+			self.removeEntity(self.entities[index])
+
 
 
 class SimpleGame(telekinesis.gamecore.GameContainer):
 	def __init__(self):
 		super(SimpleGame, self).__init__(sizeX=1000, sizeY=640)
-		self.bullet_container = BulletContainer(parent=self)
+		self.player_bullet_container = PlayerBulletContainer(parent=self)
+		self.enemy_bullet_container = EnemyBulletContainer(parent=self)
+		self.player = PlayerShip(x=480, y=580, parent=self)
 	def draw(self, screen):
 		screen.fill((0,0,0))
 		super(SimpleGame, self).draw(screen)
@@ -38,41 +51,88 @@ class CollidingEntity(telekinesis.graphics.ScreenEntity):
 		self.bounding_box = bounding_box
 		self.bounding_box.x += x
 		self.bounding_box.y += y
-		self.sx = sx
-		self.sy = sy
+		self.sx = float(sx)
+		self.sy = float(sy)
+		self.x = float(x)
+		self.y = float(y)
+		self.bounding_box_x = float(self.bounding_box.x)
+		self.bounding_box_y = float(self.bounding_box.y)
 	def update(self):
-		self.rect.x += self.sx
-		self.rect.y += self.sy
-		self.bounding_box.x += self.sx
-		self.bounding_box.y += self.sy
+		self.x += float(self.sx)
+		self.y += float(self.sy)
+		self.bounding_box_x += float(self.sx)
+		self.bounding_box_y += float(self.sy)
+		self.rect.x = int(self.x)
+		self.rect.y = int(self.y)
+		self.bounding_box.x = int(self.bounding_box_x)
+		self.bounding_box.y = int(self.bounding_box_y)
 
-
-
-class Dropship(CollidingEntity):
-	def __init__(self, *args, **kwargs):
-		super(Dropship, self).__init__(bounding_box=pygame.Rect(3, 23, 34, 14), filepath='dropship_down.png', *args, **kwargs)
-		self.health = 15
+class ShipEntity(CollidingEntity):
+	def __init__(self, health=1, max_reload=60, *args, **kwargs):
+		super(ShipEntity, self).__init__(*args, **kwargs)
+		self.health = health
+		self.reload = 0
+		self.max_reload = max_reload
+	def update(self):
+		super(ShipEntity, self).update()
+		if self.reload > 0:
+			self.reload -= 1
 	def takeDamage(self, damage):
 		self.health -= damage
 		if self.health <= 0:
+			self.onDeath()
 			self.removeSelf()
+	def onDeath(self):
+		pass
+	def fire(self):
+		self.reload = self.max_reload
 
+def calculate_adjusted_speed(x, y, speed):
+	c = (float(x) ** 2 + float(y) ** 2) ** 0.5
+	multiplier = float(speed) / c
+	return x * multiplier, y * multiplier
 
-class PlayerBullet(CollidingEntity):
+class Dropship(ShipEntity):
+	def __init__(self, *args, **kwargs):
+		super(Dropship, self).__init__(bounding_box=pygame.Rect(3, 23, 34, 14), filepath='dropship_down.png', *args, **kwargs)
+
+class DropshipFiring(Dropship):
+	def update(self):
+		super(DropshipFiring, self).update()
+		if self.reload == 0:
+			self.fire()
+	def fire(self):
+		super(DropshipFiring, self).fire()
+		player = self.parent.getGame().player
+		px, py = player.rect.x + player.rect.w / 2, player.rect.y + player.rect.h / 2
+		x, y = self.rect.x + self.rect.w / 2, self.rect.y + self.rect.h / 2
+		sx, sy = calculate_adjusted_speed(px - x, py - y, 3)
+		self.parent.getGame().enemy_bullet_container.addEntity(EnemyBullet(x=x - 10, y=y - 10, sx=sx, sy=sy))
+		
+
+class BulletEntity(CollidingEntity):
 	def __init__(self, damage=5, *args, **kwargs):
-		super(PlayerBullet, self).__init__(bounding_box=pygame.Rect(8, 3, 4, 14), filepath='player_bullet.png', *args, **kwargs)
+		super(BulletEntity, self).__init__(*args, **kwargs)
 		self.damage = damage
 	def update(self):
-		super(PlayerBullet, self).update()
+		super(BulletEntity, self).update()
 		game = self.parent.getGame()
 		if self.rect.x + self.rect.w < 0 or self.rect.x > game.sizeX or self.rect.y + self.rect.h < 0 or self.rect.y > game.sizeY:
 			self.removeSelf()
 
-class PlayerShip(CollidingEntity):
-	def __init__(self, x=0, y=0, parent=None):
-		super(PlayerShip, self).__init__(x=x, y=y, parent=parent, bounding_box=pygame.Rect(3, 3, 34, 14), filepath='dropship.png')
-		self.reload = 0
-		self.max_reload = 3
+class PlayerBullet(BulletEntity):
+	def __init__(self, *args, **kwargs):
+		super(PlayerBullet, self).__init__(bounding_box=pygame.Rect(8, 3, 4, 14), filepath='player_bullet.png', *args, **kwargs)
+
+class EnemyBullet(BulletEntity):
+	def __init__(self, *args, **kwargs):
+		super(EnemyBullet, self).__init__(bounding_box=pygame.Rect(5, 5, 10, 10), filepath='enemy_bullet_red.png', *args, **kwargs)
+
+
+
+class PlayerShip(ShipEntity):
+	def __init__(self, *args, **kwargs):
+		super(PlayerShip, self).__init__(bounding_box=pygame.Rect(3, 3, 34, 14), filepath='dropship.png', max_reload = 3, *args, **kwargs)
 	def update(self):
 		game = self.parent.getGame()
 
@@ -110,22 +170,24 @@ class PlayerShip(CollidingEntity):
 			elif self.sy > 0:
 				self.sy -= 2
 
-		if self.reload > 0:
-			self.reload -= 1
-		elif game.keystate[pygame.K_SPACE]:
-			game.bullet_container.addEntity(PlayerBullet(x=self.rect.x + self.rect.w / 2 - 10, y=self.rect.y - 20, sy=-12))
-			self.reload = self.max_reload
+		if game.keystate[pygame.K_SPACE] and self.reload == 0:
+			self.fire()
 
 		super(PlayerShip, self).update()
+
+	def fire(self):
+		super(PlayerShip, self).fire()
+		game.player_bullet_container.addEntity(PlayerBullet(x=self.rect.x + self.rect.w / 2 - 10, y=self.rect.y - 20, sy=-12))
+
 
 
 
 game = SimpleGame()
 parser = telekinesis.logic.LayoutReader(game, {
 		'Dropship': Dropship,
+		'DropshipFiring': DropshipFiring,
 		'DelayedSpawn': telekinesis.logic.DelayedSpawn,
 	})
 parser.fromFile('simple_game.layout')
-game.addEntity(PlayerShip())
 game.run()
 
